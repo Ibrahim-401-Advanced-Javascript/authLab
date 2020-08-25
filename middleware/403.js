794e502303d93bce6bacac27a32bd5d1a414947f
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-08-25 10:16:50 -0700

    added tests and modified authorization middleware

--- middleware/403.js
+++ middleware/403.js
@@ -3,5 +3,5 @@
 module.exports = (error, req, res) => {
   res.status(403);
   res.statusMessage = 'Error Creating User';
-  res.json({error:error});
+  res.json({error: 'Error Creating User'});
 };
--- src/auth/users/users.js
+++ src/auth/users/users.js
@@ -4,11 +4,14 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-// users schema
+// users schema -----
 const users = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: String, required: true }, 
+  email:{ type: String, required: true }
 });
+// ------------------
 
 //modify user instance before saving
 users.pre('save', async function() {
@@ -37,7 +40,7 @@ users.methods.comparePassword = function(inputPass) {
         return null;
       }
     })
-
+    .catch(handle401);
 };
 
 users.methods.getToken = function () {
